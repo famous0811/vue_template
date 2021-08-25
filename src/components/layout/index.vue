744eb820c603070fff72ae6b67d040a7907cf3e4
allblack <umh0811@naver.com>
2021-08-25 10:02:34 +0900

    update asset & layout & global styleAsset

---
+++ src/components/layout/index.vue
@@ -0,0 +1,27 @@
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+<script lang="ts">
+@Options({
+    name: "exmaple",
+    props:{
+
+    },
+    components:{
+
+    },
+    methods: {}
+})
+export default class Layout extends Vue {
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+<style lang="scss">
+    
+</style>
---
+++ src/pages/example/example.vue
@@ -0,0 +1,29 @@
+<template lang="">
+    <div>
+        <div>example.vue</div>
+    </div>
+</template>
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+    name: "exmaple",
+    props:{
+
+    },
+    components:{
+
+    },
+    methods: {}
+})
+export default class Example extends Vue {
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+<style lang="scss">
+    
+</style>
