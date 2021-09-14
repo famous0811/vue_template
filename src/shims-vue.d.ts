1bbc6a097dc67dc4f6292d50644782a714389a55
allblack <umh0811@naver.com>
2021-09-14 11:42:36 +0900

    fix import img

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,13 @@
 /* eslint-disable */
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+
+declare module "*.jpg" {
+  export default "" as string;
+}
+declare module "*.png" {
+  export default "" as string;
 }
